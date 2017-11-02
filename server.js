4adddf532eaf2be3538b1992601214c3e18707da
fcaceres <fcaceres@portalfinance.co>
2017-11-02 15:59:49 -0500

    fake module

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-//const router = jsonServer.router('db.json');
 const router = jsonServer.router('db.json');
+const mock = require('./db-api.js');
+console.log(mock.generateAgents);
+//console.log(router);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
