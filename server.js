e6725937ecad2141d3ab1475ffd08e1beb311fc8
fcaceres <fcaceres@portalfinance.co>
2017-11-03 09:56:52 -0500

    build file db

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const jsonServer = require('json-server');
+const fs = require("fs");
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const mock = require('./db-api.js');
-console.log(mock.generateAgents);
-//console.log(router);
+const m = require('./db-api.js');
+
+output = m.data();
+
+fs.writeFile("db.json",JSON.stringify(output), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
+
+console.log(output);
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
