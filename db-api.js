03f1f787dd13a5643eb443371d861357ba4b5ba1
fcaceres <fcaceres@portalfinance.co>
2017-11-17 19:14:35 -0500

    update api fake

--- db-api.js
+++ db-api.js
@@ -6,129 +6,204 @@ module.exports = {
         let agents = [];
         let companies = [];
         let permissions = [];
+        let matrix = [];
+        let debts = [];
+        let graph1 = [];
+        let graph2 = [];
+        let graph3 = [];
+        let months = ["2016-10", "2016-11", "2016-12", "2017-01", "2017-02", "2017-03", "2017-04", "2017-05", "2017-08", "2017-09", "2017-10", "2017-11"];      
       
         for (let id = 0; id < 100; id++){
-          let first_name = faker.name.firstName();
-          let last_name = faker.name.lastName();
-          let branch = faker.random.word();
-          let phone = faker.phone.phoneNumber();
-          let email = faker.internet.email();
-          let company = faker.company.companyName();
-          let image = faker.image.avatar();
+            let first_name = faker.name.firstName();
+            let last_name = faker.name.lastName();
+            let branch = faker.random.word();
+            let phone = faker.phone.phoneNumber();
+            let email = faker.internet.email();
+            let company = faker.company.companyName();
+            let image = faker.image.avatar();
+        
+            agents.push({
+              "id": id,
+              "first_name": first_name,
+              "last_name": last_name,
+              "branch": branch,
+              "phone": phone,
+              "email": email,
+              "company": company,
+              "image": image,
+              "permissions": [],
+            });
+          }
+        
+          for (let id = 0; id < 200; id ++){
+            let agent_id = Math.floor(Math.random() * 101);
+            let name = faker.company.companyName()
+            let suffix = faker.company.companySuffix()
+            let address = faker.address.streetAddress()
+            let phone = faker.phone.phoneNumber();
+            let tax_number = faker.finance.account();
+            let city = faker.address.city();
+            let post_code = faker.address.zipCode();
+            let country = faker.address.country();
+        
+            companies.push({
+              "id": id,
+              "name": name,
+              "phone": phone,
+              "address": address,
+              "suffix": suffix,
+              "agentId": agent_id,
+              "tax_number": tax_number,
+              "post_code": post_code,
+              "city": city,
+              "country": country
+            });
+        
+          }
+        
+        
+          for (let c = 0 ; c <100;c++){
+              let id = c+1;
+              let c_company = faker.company.companyName();
+              let c_rpr = faker.random.boolean();
+              let c_roc = faker.random.boolean();
+              let c_vb = faker.random.boolean();
+        
+            matrix.push({
+                "id":id,
+                "company_name":c_company,
+                "require_pvr": c_rpr,
+                "require_po" :c_roc,
+                "require_ok":c_vb,
+                "num_executives":"0",
+                "executive_1":"",
+                "executive_2":""
+            });
+          }
+        
+          for (let d = 0; d<100;d++){
+            let id = d+1;
+            let d_supplier_name = faker.company.companyName();
+            let d_invoice_num = faker.random.uuid();
+            let d_invoice_amount = faker.commerce.price();
+            let d_ok_pay = faker.random.boolean();
+            let d_ceded = faker.random.boolean();
+            let d_ceded_to = '';
+        
+            if (d_ceded){
+                d_ceded_to = faker.company.companyName();
+            }
+        
+        
+            debts.push({
+                "id": id,
+                "supplier_name":d_supplier_name,
+                "invoice_num":d_invoice_num,
+                "invoice_amount": d_invoice_amount,
+                "ok_pay": d_ok_pay,
+                "ceded":d_ceded,
+                "ceded_to":d_ceded_to
+            });
+          }
+        
+          for (let i = 0; i < 12; i ++) {
+            let mon = Math.floor(Math.random() * 2000000);
+            let cant = Math.floor(Math.random() * 8);
+            item = {
+              "date": months[i],
+              "monto": mon,
+              "cantidad": cant,
+            },
+            graph1.push(item);
+          }
+        
+          for (let i = 0; i < 12; i ++) {
+            let cant = Math.floor(Math.random() * 250);
+            item = {
+              "date": months[i],
+              "clients": cant,
+            },
+            graph2.push(item);
+          }
       
-          agents.push({
-            "id": id,
-            "first_name": first_name,
-            "last_name": last_name,
-            "branch": branch,
-            "phone": phone,
-            "email": email,
-            "company": company,
-            "image": image,
-            "permissions": [],
-          });
-        }
       
-        for (let id = 0; id < 200; id ++){
-          let agent_id = Math.floor(Math.random() * 101);
-          let name = faker.company.companyName()
-          let suffix = faker.company.companySuffix()
-          let address = faker.address.streetAddress()
-          let phone = faker.phone.phoneNumber();
-          let tax_number = faker.finance.account();
-          let city = faker.address.city();
-          let post_code = faker.address.zipCode();
-          let country = faker.address.country();
-      
-          companies.push({
-            "id": id,
-            "name": name,
-            "phone": phone,
-            "address": address,
-            "suffix": suffix,
-            "agentId": agent_id,
-            "tax_number": tax_number,
-            "post_code": post_code,
-            "city": city,
-            "country": country
-          });
-      
-        }
-      
-      
-        return data =  {
-          "agents": agents,
-          "companies": companies,
-          "permissionsAll": [
-            "ViewDashboardInvoices",
-            "ViewOffers",
-            "ExecuteTransactions",
-            "ViewAgents",
-            "GenerateOffers",
-            "CreateAgents",
-            "ViewSuppliers",
-            "ApproveRisks",
-            "TransactionsLogs",
-            "ReAssignInvoice",
-          ],
-          "permissions":[{
-                  "id": "8677815a-a84a-47a3-829d-b860ff188e0c",
+          return {
+            "graph1": graph1,
+            "graph2": graph2,
+            "agents": agents,
+            "matrix": matrix,
+            "debtspay": debts,
+            "companies": companies,
+            "permissionsAll": [
+              "ViewDashboardInvoices",
+              "ViewOffers",
+              "ExecuteTransactions",
+              "ViewAgents",
+              "GenerateOffers",
+              "CreateAgents",
+              "ViewSuppliers",
+              "ApproveRisks",
+              "TransactionsLogs",
+              "ReAssignInvoice",
+            ],
+            "permissions":[{
+                    "id": "432b7c9d-b474-4403-9c55-14a8ef89dbcc",
+                    "type":"debtor",
+                    "permissions":[
+                        "ViewDashboardInvoices",
+                        "ViewSuppliers",
+                        "ViewAgents",
+                        "ViewOPMatrix",
+                        "ViewDebtsPay",
+                        "ViewAprovals"
+                    ]
+                },{
+                  "id": "36ea0f1f-8b62-4f4c-88b1-f63762819381",
                   "type":"supplier",
                   "permissions":[
                       "ViewDashboardInvoices",
                       "ViewOffers",
                       "ExecuteTransactions",
-                      "ViewAgents"
                   ]
               },{
-                "id": "67c0e2d6-b3b9-46f2-b4e9-d75e5b2dccd3",
-                "type":"supplier",
-                "permissions":[
-                    "ViewDashboardInvoices",
-                    "ViewOffers",
-                    "ExecuteTransactions",
-                    "ViewAgents",
-                    "ViewSuppliers",
-                ]
-            },{
-                  "id":"7d41327b-490c-4015-a67a-1a1b4a35a623",
-                  "type":"financial",
-                  "permissions":[
-                      "GenerateOffers",
-                      "CreateAgents",
+                    "id":"157bb9fd-ebb7-4602-8f92-c974cb97da1a",
+                    "type":"financial",
+                    "permissions":[
+                      "ViewDashboardInvoices",
                       "ViewSuppliers",
-                      "ApproveRisks",
-                      "TransactionsLogs",
-                      "ReAssignInvoice",
-                      "ViewSuppliers",
-                      "ViewAgents"
-                  ]
-              },{
-                  "id": "b09fb980-edd3-4c59-9bb9-173e51f99dcd",
-                  "type":"debtor",
-                  "permissions":[
-                      "GenerateOffers",
-                      "ViewSuppliers",
-                      "TransactionsLogs",
-                      "ViewAgents"
-                  ]
-              }],
-          "appconfig":[
-              {
-              "subdomain":"nuevocapital",
-              "tenantid":"12345678",
-              "financialid":"7890"
-              },{
-              "subdomain":"motorfinanciero",
-              "tenantid":"12345678",
-              "financialid":"7890"
-              },{
-              "subdomain":"btg",
-              "tenantid":"12345678",
-              "financialid":"7890"
-              }
-          ]
-        };
+                      "ViewAgents",
+                      "ViewOPMatrix",
+                      "ViewDebtsPay",
+                      "ViewAprovals",
+                      "ViewOffers",
+                      "ExecuteTransactions",
+                    ]
+                },{
+                    "id": "b09fb980-edd3-4c59-9bb9-173e51f99dcd",
+                    "type":"debtor",
+                    "permissions":[
+                        "GenerateOffers",
+                        "ViewSuppliers",
+                        "TransactionsLogs",
+                        "ViewAgents"
+                    ]
+                }],
+            "appconfig":[
+                {
+                "subdomain":"nuevocapital",
+                "tenantid":"12345678",
+                "financialid":"7890"
+                },{
+                "subdomain":"motorfinanciero",
+                "tenantid":"12345678",
+                "financialid":"7890"
+                },{
+                "subdomain":"btg",
+                "tenantid":"12345678",
+                "financialid":"7890"
+                }
+            ]
+          };
        
     }
 }
